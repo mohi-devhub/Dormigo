8ff2ebcc8743eedfb475f603b3f6ecf2a6ac8f3c
Jenson-77 <jensonrajan321@gmail.com>
2025-09-03 20:16:20 +0530

    backend-commit

---
+++ security/src/main/java/com/jwtsecurity/security/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jwtsecurity.security.user;
+import com.jwtsecurity.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
---
+++ security/src/test/java/com/jwtsecurity/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtsecurity.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
